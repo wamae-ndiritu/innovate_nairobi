2d3156115bcfb560ce2d6cb0a61e50cecc1b3a2e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-09 01:43:38 +0300

    Rebuild services component

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { servicesData } from "../data/services";
 
 const Services = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-3 col-md-4 col-sm-6">Card</div>
-        <div className="col-lg-3 col-md-4 col-sm-6">Card</div>
-        <div className="col-lg-3 col-md-4 col-sm-6">Card</div>
-        <div className="col-lg-3 col-md-4 col-sm-6">Card</div>
+    <div className="container mt-5">
+      <div className="sub-title">
+        <h4 className="h4 text-center">Get Emergency Support Services!</h4>
+        <div className="hr"></div>
+      </div>
+      <div className="row mt-3">
+        {servicesData.map((service) => {
+          const { id, title, image, desc } = service;
+          return (
+            <div className="col-lg-3 col-md-4 col-sm-6 service-card" key={id}>
+              <div className="card">
+                <div className="service-img">
+                  <img src={image} alt="..." />
+                </div>
+                <h5 className="h5">{title}</h5>
+                <p className="">{desc}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ client/src/data/services.js
@@ -0,0 +1,26 @@
+export const servicesData = [
+  {
+    id: 0,
+    title: "Fire Emergencies",
+    image: "/assets/fire.jpg",
+    desc: "Our Rapid Fire Emergency Response Services are meticulously designed to swiftly address any fire-related crisis.",
+  },
+  {
+    id: 1,
+    title: "Road Accidents",
+    image: "/assets/accident.jpg",
+    desc: "Our Swift Road Accident Assistance service provides immediate support when accidents strike, ensuring a rapid and well-coordinated response to any roadway incident.",
+  },
+  {
+    id: 2,
+    title: "Health Emergencies",
+    image: "/assets/health.jpg",
+    desc: "Our Urgent Health Emergency Support service is your lifeline in critical moments, delivering swift and compassionate assistance when health crises arise.",
+  },
+  {
+    id: 3,
+    title: "Empowerment Against Sexual Harassment",
+    image: "/assets/sexual.jpg",
+    desc: "Our Empowerment Against Sexual Harassment initiative is dedicated to creating a safe and inclusive environment by taking a stand against all forms of sexual harassment.",
+  },
+];
