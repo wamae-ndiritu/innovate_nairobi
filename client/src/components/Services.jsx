b4c2ce1dcdc99751f275da19cf79e31b73292ad2
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-08 14:56:17 +0300

    Add Service component

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -7,23 +7,63 @@ const Services = () => {
         <h6 className="h6 text-center sub-title  my-3">
           Get Responses for your Emergencies!
         </h6>
-        <div className="row">
+        <div className="row my-3">
           <div className="col-lg-3 col-md-4 col-sm-6">
-            <div className="service-card shadow-sm">
+            <div className="card">
               <div className="service-img">
                 <img src="/assets/call-center.png" alt="..." />
               </div>
-              <h6 className="service-title">Ambulance</h6>
-              <p>
-                Jelly sweet roll jelly beans biscuit pie macaroon chocolate
-                donut. Carrot cake caramels pie sweet apple pie tiramisu carrot
-                cake.
-              </p>
+              <div className="card-body">
+                <h5 className="card-title text-center">Ambulance</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-sm-6">card</div>
-          <div className="col-lg-3 col-md-4 col-sm-6">card</div>
-          <div className="col-lg-3 col-md-4 col-sm-6">card</div>
+          <div className="col-lg-3 col-md-4 col-sm-6">
+            <div className="card">
+              <div className="service-img">
+                <img src="/assets/call-center.png" alt="..." />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center">Ambulance</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6">
+            <div className="card">
+              <div className="service-img">
+                <img src="/assets/call-center.png" alt="..." />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center">Ambulance</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6">
+            <div className="card">
+              <div className="service-img">
+                <img src="/assets/call-center.png" alt="..." />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center">Ambulance</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
