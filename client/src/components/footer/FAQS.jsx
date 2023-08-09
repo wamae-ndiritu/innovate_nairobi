5a8436dd12eaef5d4693c6316c4777cc5031653f
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-09 14:57:24 +0300

    Add footer

---
+++ client/src/components/footer/FAQS.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FAQS = () => {
+  return (
+    <div>
+      <section>
+        <h2>How do I get access to emergency services?</h2>
+        <p>
+          To request emergency services, open the app and press "call for help".
+        </p>
+      </section>
+      <section>
+        <h2>Are we available 24/7?</h2>
+        <p>Yes! We are available 24/7 ,7 days a week.</p>
+      </section>
+    </div>
+  );
+};
+
+export default FAQS;
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import "./footer.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 const Footer = () => {
-
-    return(
-        <footer className="app-footer">
-        <div className="footer-top">
-          <div className="footer-logo">
-            <img src="logo192.png" alt="Emergency Service App Logo"/>
-          </div>
-          <div className="footer-links">
-            <h3>Quick Links</h3>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Support</a></li>
-            </ul>
-          </div>
+  return (
+    <footer className="app-footer">
+      <div className="footer-top">
+        <div className="footer-logo">
+          <img src="logo192.png" alt="Emergency Service App Logo" />
         </div>
-        <div className="faqs">
-          <p>      </p>
+        <div className="footer-links">
+          <h3>Quick Links</h3>
+          <ul>
+            <li>
+              <Link to="#">Home</Link>
+            </li>
+            <li>
+              <Link to="#">Support</Link>
+            </li>
+            <li>
+              <Link to="/faqs">FAQS</Link>
+            </li>
+          </ul>
         </div>
-      </footer>
-    )
+      </div>
+      <div className="faqs">
+        <p> </p>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
