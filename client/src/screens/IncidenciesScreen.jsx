8cb59977ae1ff01f4884ebf6e1fc5d64776ac3c6
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-09 03:44:56 +0300

    Add responsiveness to icident page

--- client/src/screens/IncidenciesScreen.jsx
+++ client/src/screens/IncidenciesScreen.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Incident from "../components/Incident/Incident";
+import Title from "../components/Title";
 
 const IncidenciesScreen = () => {
   return (
-    <>
-      <Incident />
-    </>
+    <div className="incidence-screen mt-3">
+      <Title>Check Incidences Reported!</Title>
+      <div className="incidence-screen-wrapper">
+        <div className="incidence-filters">
+          <h6 className="h6">Filter Incidences Reported</h6>
+          <div className="input">
+            <input type="text" placeholder="search location..." />
+            <button>Search</button>
+          </div>
+          <div className="time-sort">
+            <label>Sort By Time</label>
+            <select>
+              <option>Latest Incidence</option>
+              <option>Old Incidence</option>
+            </select>
+          </div>
+        </div>
+        <div className="incidence-list">
+          <Incident />
+        </div>
+      </div>
+    </div>
   );
 };
 
