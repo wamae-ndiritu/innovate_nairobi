b0ca6dea0938c96e41c847d887849fca698632a4
Sammy <samuel.maina1@student.moringaschool>
2023-08-10 17:35:39 +0300

    http get request

--- client/src/components/Express.js
+++ client/src/components/Express.js
@@ -1,4 +1,12 @@
-javascript
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // Do something with the response data
+  })
+  .catch(error => {
+    // Handle any errors that occur during the request
+  });
+  
 fetch(url)
   .then(response => response.json())
   .then(data => {
